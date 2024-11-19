e87764ea6dbb16529ca817a778f0ef4d42172ec5
Tobi <tobi_adewuyi@yahoo.com>
2024-11-19 17:08:50 +0000

    Make UI more mobile-friendly

--- frontend/src/components/page.tsx
+++ frontend/src/components/page.tsx
@@ -6,12 +6,12 @@ export default function Page() {
     <div className="flex flex-col items-center justify-center min-h-screen p-4 bg-black">
       <div className="grid grid-cols-1 sm:grid-cols-2">
         {/* LED Grid Container */}
-        <div className="flex flex-1 bg-gray-200 p-4 rounded-lg shadow-md m-2 min-w-96 w-[35vw] aspect-square">
+        <div className="flex flex-1 bg-gray-200 p-4 rounded-lg shadow-md m-2 min-w-72 md:min-w-96 aspect-square">
           {/* <h2 className="text-xl font-bold mb-4">LED Grid</h2> */}
           <LEDGrid />
         </div>
         {/* Twitch Player Container */}
-        <div className="flex flex-1 bg-gray-200 p-4 rounded-lg shadow-md m-2 min-w-96 w-[35vw] aspect-square">
+        <div className="flex flex-1 bg-gray-200 p-4 rounded-lg shadow-md m-2 min-w-72 md:min-w-96 aspect-square">
           {/* <h2 className="text-xl font-bold mb-4">Twitch Player</h2> */}
           <TwitchStream />
         </div>
