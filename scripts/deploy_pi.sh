19922e2f8feae6acb3a85a399c5635192d5279fa
Tobi Adewuyi <tobi_adewuyi@yahoo.com>
2024-12-06 21:32:48 +0000

    some more script changes

--- scripts/deploy_pi.sh
+++ scripts/deploy_pi.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -euo pipefail
-./register_service.sh "/home/tobi/Development/upnepa/systemd/led_controller.service"
-./register_service.sh "/home/tobi/Development/upnepa/systemd/video_feed.service"
+scripts/register_service.sh "led_controller.service" "/home/tobi/Development/upnepa/systemd/led_controller.service"
+scripts/register_service.sh "video_feed.service" "/home/tobi/Development/upnepa/systemd/video_feed.service"
--- scripts/deploy_remote.sh
+++ scripts/deploy_remote.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -euo pipefail
-./register_service.sh "/home/tobi/Development/upnepa/systemd/mediamtx.service"
-./register_service.sh "/home/tobi/Development/upnepa/systemd/socket_server.service"
+./register_service.sh "mediamtx.service" "/home/tobi/Development/upnepa/systemd/mediamtx.service"
+./register_service.sh "socket_server.service" "/home/tobi/Development/upnepa/systemd/socket_server.service"
